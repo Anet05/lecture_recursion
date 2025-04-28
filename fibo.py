4ad8ae72adc9434491781e3b59706c60fcdd7784
Aneta Langerova <aneta.langerova05@seznam.cz>
2025-04-28 14:05:35 +0200

    message

--- fibo.py
+++ fibo.py
@@ -1,5 +1,18 @@
+def recursive_nth_fibo(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return recursive_nth_fibo(n-1) + recursive_nth_fibo(n-2)
+
+
 
 def main():
+    n = int(input("Pořadí členu: "))
+    seq = [recursive_nth_fibo(num) for num in range(n+1)]
+    print(seq)
+    print(recursive_nth_fibo(n))
     pass
 
 
